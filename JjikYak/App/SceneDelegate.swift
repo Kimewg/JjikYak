e1dec5e68f88d373ccd92439b4eff69032d8e535
eunseo <sawol2239@gmail.com>
2026-02-25 17:35:37 +0900

    [Fix] SceneDelegate Gemini API 테스트 로직 에러 해결

--- JjikYak/App/SceneDelegate.swift
+++ JjikYak/App/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         print("API키 확인 : \(ConfigManager.geminiAPIKey)")
         
         let testService = GeminiService()
-            
+        testService.parsePillInfo(ocrText: "타이레놀 8시간 이알 서방정, 식후 30분 복용, 비타민 A, 비타민D") { result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let pillInfos):
@@ -47,7 +47,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 case .failure(let error):
                     print("❌ 통신 또는 디코딩 에러 발생: \(error)")
                 }
-        testService.parsePillInfo(ocrText: "타이레놀 8시간 이알 서방정, 식후 30분 복용, 비타민 A, 비타민D") { result in
             }
         }
         
